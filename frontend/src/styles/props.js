73c70084cf3ea7292c6886d384812e99006899e4
lineJeong <line.jeong32@gmail.com>
2022-09-22 22:38:07 +0900

    FEAT: 재사용 버튼 컴포넌트 작업 완료

---
+++ frontend/src/styles/props.js
@@ -0,0 +1,27 @@
+export const buttonStyles = {
+    mdGreenButton: "bg-wagmiGreen text-textWhite font-medium rounded-lg p-3",
+    smGreenButton:
+      "bg-wagmiGreen text-textWhite text-sm font-medium rounded-lg py-2 px-4",
+    smLightGreenButton:
+      "bg-wagmiLightGreen text-textWhite text-sm font-medium rounded-lg py-2 px-4",
+    smWhiteButton: "bg-white text-sm font-medium rounded-lg py-2 px-4",
+    lgGreenSemiRoundButton:
+      "bg-wagmiGreen text-textWhite text-lg font-medium rounded-3xl py-2 px-5",
+    mdTextGreenButton: "text-wagmiGreen p-2",
+    mdTextGrayButton: "text-textGray p-2",
+    smTextBlackButton: "text-sm font-medium p-2",
+    xsTextGrayButton: "text-textGray text-xs p-2",
+    loginButton: "text-textWhite font-medium leading-none whitespace-nowrap mr-4",
+  
+    // svg icon button
+    changeButton:
+      "bg-wagmiLightGreen rounded-full h-7 w-7 inline-block align-middle",
+    gpsButton: "bg-wagmiGreen rounded-full h-[45px] w-[45px]",
+    upButton: "bg-wagmiLightGreen rounded-full h-[60px] w-[60px] drop-shadow-up",
+    backButton: "pr-[18px]",
+    searchButton: "p-[13px] m-[-13px]",
+    kakaoLoginButton:
+      "flex items-center w-[100%] py-3 px-5 rounded-lg bg-[#fbe64d] mt-8",
+    hambergerButton: "",
+  };
+  
