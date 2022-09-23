edc4fd3810770843d0506387d5a4b4e85eca0471
lineJeong <line.jeong32@gmail.com>
2022-09-23 19:59:27 +0900

    FEAT: 재사용 카드 컴포넌트 작업 완료

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,5 +1,17 @@
-function Card() {
-  return <div>Card</div>;
+function Card({ id, emoji, name, option = null, style, isOneLine = false }) {
+  return (
+    <li className={`${style} text-sm`} key={id}>
+      <span className={isOneLine ? null : "text-2xl"}>{emoji}</span>
+      {isOneLine ? (
+        <span className="ml-2">{name}</span>
+      ) : (
+        <div className="py-[4px]">{name}</div>
+      )}
+      <span className={isOneLine ? "float-right" : "text-xs text-textGray"}>
+        {option}
+      </span>
+    </li>
+  );
 }
 
 export default Card;
