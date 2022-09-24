6fded7dd8384225a3135307695eadf4642cd5791
lineJeong <line.jeong32@gmail.com>
2022-09-24 23:44:13 +0900

    FEAT: 테마 등록 요청 페이지 레이아웃 작업 완료

--- frontend/src/pages/Request.js
+++ frontend/src/pages/Request.js
@@ -1,5 +1,61 @@
+import Button from "components/Button";
+import Header from "components/Header";
+import { useState } from "react";
+import { buttonStyles } from "styles/props";
+
 function Request() {
-  return <div>Request</div>;
+  const [request, setRequest] = useState({ title: "", content: "" });
+  const { title, content } = request;
+
+  const { mdGreenButton, mdWhiteButton } = buttonStyles;
+
+  const handleRequestChange = (e) => {
+    const { name, value } = e.target;
+    setRequest({ ...request, [name]: value });
+  };
+
+  const handleRequestSubmit = () => {
+    console.log(request);
+  };
+
+  return (
+    <div className="relative">
+      <Header />
+      <div className="padding-container">
+        <div className="font-medium text-center mb-6">
+          새로운 테마를 등록하고 싶으신가요?
+        </div>
+        <div className="bg-white rounded-lg border border-borderGray p-[13px] mb-5">
+          <input
+            className="text-sm w-full"
+            type="text"
+            placeholder="테마의 이름을 추천해주세요."
+            name="title"
+            value={title}
+            onChange={(e) => handleRequestChange(e)}
+          />
+        </div>
+        <div className="bg-white rounded-lg border border-borderGray p-[13px] mb-5">
+          <textarea
+            className="text-sm w-full h-60 resize-none"
+            name="content"
+            placeholder="나만의 장소를 소개할 수 있는 유용한 테마를 추가할 수 있습니다.&#10;테마를 설명할 수 있는 소개글을 적어주세요.&#10;ㅤ&#10;• 요청하신 테마는 확인 후 빠르게 등록됩니다.&#10;• 약간 변경되어 등록될 수 있는 점 양해해주세요!"
+            value={content}
+            onChange={(e) => handleRequestChange(e)}
+          />
+        </div>
+        <div className="text-center">
+          <Button name="취소하기" style={mdWhiteButton} />
+          <span className="ml-4" />
+          <Button
+            name="테마 추천하기"
+            handleButtonClick={handleRequestSubmit}
+            style={mdGreenButton}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Request;
