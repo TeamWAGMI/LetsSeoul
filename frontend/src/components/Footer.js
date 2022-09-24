604613de097d39d13f5601c9790b1f2405cee0ca
lineJeong <103614912+lineJeong@users.noreply.github.com>
2022-09-24 17:13:14 +0900

    FEAT: 푸터 컴포넌트 작업 완료 (#30)

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,16 @@
-function Footer() {
-  return <div>Footer</div>;
-}
-
+const Footer = () => {
+  return (
+    <footer className="text-center py-[60px] text-sm leading-none">
+      <div className="mb-4">
+        <span className="mx-4">Facebook</span>
+        <span className="mx-4">Instagram</span>
+        <span className="mx-4">E-Mail</span>
+      </div>
+      <div>
+        <span className="mx-4">서비스 이용약관</span>
+        <span className="mx-4">개인정보처리방침</span>
+      </div>
+    </footer>
+  );
+};
 export default Footer;
