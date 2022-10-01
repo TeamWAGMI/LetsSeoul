f2e8e48e6b121787b5a16e98eb1eb8a3986338b5
lineJeong <line.jeong32@gmail.com>
2022-10-01 22:49:23 +0900

    FEAT: 유저 정보 페이지 닉네임, 이모지, 한줄소개 받아오기 완료

--- frontend/src/pages/UserInfo.js
+++ frontend/src/pages/UserInfo.js
@@ -1,25 +1,48 @@
 import Button from "components/Button";
 import Card from "components/Card";
 import Header from "components/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { userPicktheme } from "static/dummyData";
 import { buttonStyles } from "lib/styles";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 function UserInfo() {
-  const [inputText, setInputText] = useState({ name: "유저명", intro: "" });
+  const [userProfile, setUserProfile] = useState({
+    emoji: "",
+    nickname: "",
+    introduction: "",
+  });
+  const { emoji, nickname, introduction } = userProfile;
+
   const [isEditable, SetIsEditable] = useState(false);
   const navigate = useNavigate();
-  const uid = 1;
+  const { uid } = useParams();
 
   const {
     smTextBlackButton,
     smWhiteButton,
+    smGrayButton,
     smLightGreenButton,
     mdGreenButton,
     mdWhiteGreenButton,
     refreshButton,
   } = buttonStyles;
+
+  useEffect(() => {
+    axios
+      .get(`/api/v1/users/${uid}`)
+      .then((res) => setUserProfile((prev) => ({ ...prev, ...res.data })));
+  }, [uid]);
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserProfile((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleEditButton = () => {
+    SetIsEditable((prev) => !prev);
+  };
 
   return (
     <>
@@ -29,7 +52,7 @@ function UserInfo() {
           <div className="smHeadline">내 정보 관리</div>
           <div className="flex justify-between mb-3">
             <div className="bg-white rounded-full w-24 h-24 p-6 relative">
-              <span className="text-5xl">🐹</span>
+              <span className="text-5xl">{emoji}</span>
               <Button icon="refresh" styles={refreshButton} />
             </div>
             <div className="flex">
@@ -42,19 +65,11 @@ function UserInfo() {
                     handleButtonClick={() => navigate(`/user/${uid}/followers`)}
                   />
                 </div>
-                {isEditable ? (
-                  <Button
-                    name="수정 완료"
-                    styles={smWhiteButton}
-                    handleButtonClick={() => SetIsEditable(false)}
-                  />
-                ) : (
-                  <Button
-                    name="프로필 수정"
-                    styles={smWhiteButton}
-                    handleButtonClick={() => SetIsEditable(true)}
-                  />
-                )}
+                <Button
+                  name={isEditable ? "수정 완료" : "프로필 수정"}
+                  styles={isEditable ? smGrayButton : smWhiteButton}
+                  handleButtonClick={handleEditButton}
+                />
               </div>
               <div className="flex flex-col justify-between">
                 <div className="text-right leading-none">
@@ -74,24 +89,26 @@ function UserInfo() {
           <div className="flex flex-col">
             <input
               disabled={!isEditable}
+              name="nickname"
               type="text"
               maxLength="10"
-              className="w-[160px] h-[34px] rounded-lg p-[10px] text-sm leading-none font-normal text-black mb-3 bg-white"
-              value={inputText.name}
-              onChange={(e) => {
-                setInputText({ ...inputText, name: e.target.value });
-              }}
+              className={`w-[160px] h-[34px] rounded-lg p-[10px] text-sm leading-none mb-3 bg-white ${
+                isEditable ? "focus:ring focus:ring-wagmiLightGreen" : null
+              }`}
+              value={nickname}
+              onChange={handleChangeInput}
             />
             <input
               disabled={!isEditable}
+              name="introduction"
               type="text"
               placeholder="한줄 소개를 입력하세요."
               maxLength="20"
-              className="w-[280px] h-[34px] rounded-lg p-[10px] text-sm leading-none bg-white"
-              value={inputText.intro}
-              onChange={(e) => {
-                setInputText({ ...inputText, intro: e.target.value });
-              }}
+              className={`w-[280px] h-[34px] rounded-lg p-[10px] text-sm leading-none bg-white ${
+                isEditable ? "focus:ring focus:ring-wagmiLightGreen" : null
+              }`}
+              value={introduction}
+              onChange={handleChangeInput}
             />
           </div>
         </div>
