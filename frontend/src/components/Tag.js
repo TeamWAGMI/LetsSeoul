e6f1a7848b8a20c0dcd960cebbffc0e258441f49
lineJeong <103614912+lineJeong@users.noreply.github.com>
2022-10-05 18:34:24 +0900

    Feat/fe/th 0009 (이슈 #142, 키워드 및 태그를 통한 테마 검색 기능 구현) (#185)
    * FIX: API response 데이터명 변경에 따라 수정
    
    * REFACTOR: 동일 카테고리 내 태그 다중 선택 가능하도록 수정
    
    * FEAT: 키워드 및 태그를 통한 검색 기능 구현 완료

--- frontend/src/components/Tag.js
+++ frontend/src/components/Tag.js
@@ -1,13 +1,18 @@
 function Tag({ title, tags, selectedTag, setSelectedTag }) {
-  const tagClass = (id) => {
-    if (selectedTag === id)
-      return "bg-wagmiLightGreen rounded-lg text-white font-semibold text-xs leading-none p-[10px] mr-[10px] mb-[10px]";
-    return "bg-white rounded-lg text-[#756E56] font-semibold text-xs leading-none p-[10px] mr-[10px] mb-[10px] hover:bg-[#E6E3D6]";
+  const tagClass = (name) => {
+    return `${
+      selectedTag.includes(name)
+        ? "bg-wagmiLightGreen text-white"
+        : "bg-white text-[#756E56] hover:bg-[#E6E3D6]"
+    } rounded-lg text-xs font-semibold p-[10px] mr-[10px] mb-[10px] leading-none`;
   };
 
-  const handleTagSelect = (id) => {
-    setSelectedTag(id);
-    if (selectedTag === id) setSelectedTag(null);
+  const handleTagSelect = (name) => {
+    setSelectedTag((prev) => [...prev, name]);
+
+    if (selectedTag.includes(name)) {
+      setSelectedTag(selectedTag.filter((tag) => tag !== name));
+    }
   };
 
   return (
@@ -16,9 +21,9 @@ function Tag({ title, tags, selectedTag, setSelectedTag }) {
       <div>
         {tags.map((tag) => (
           <button
-            key={tag.id}
-            onClick={() => handleTagSelect(tag.id)}
-            className={tagClass(tag.id)}
+            key={tag.name}
+            onClick={() => handleTagSelect(tag.name)}
+            className={tagClass(tag.name)}
           >
             {tag.name}
           </button>
--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,18 +1,22 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import Button from "components/Button";
 import Card from "components/Card";
+import Footer from "components/Footer";
 import SearchBar from "components/SearchBar";
 import Tag from "components/Tag";
 import { scrollToTop } from "lib/utils/scrollToTop";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { atWhere, doWhat, popularThemes, withWhom } from "static/dummyData";
+import { atWhere, doWhat, withWhom } from "static/dummyData";
 import { buttonStyles, cardStyles } from "lib/styles";
-import Footer from "components/Footer";
 
 function Search() {
-  const [selectedWhom, setSelectedWhom] = useState(null);
-  const [selectedWhat, setSelectedWhat] = useState(null);
-  const [selectedWhere, setSelectedWhere] = useState(null);
+  const [themeCardsList, setThemeCardsList] = useState([]);
+  const [selectedWhom, setSelectedWhom] = useState([]);
+  const [selectedWhat, setSelectedWhat] = useState([]);
+  const [selectedWhere, setSelectedWhere] = useState([]);
+  const [inputKeyword, setInputKeyword] = useState("");
+  const [submitKeyword, setSubmitKeyword] = useState("");
 
   const { gridThemeCard } = cardStyles;
   const { mdGreenButton } = buttonStyles;
@@ -21,11 +25,28 @@ function Search() {
     scrollToTop();
   }, []);
 
+  useEffect(() => {
+    const data = {
+      keyword: submitKeyword,
+      who: selectedWhom,
+      what: selectedWhat,
+      where: selectedWhere,
+    };
+
+    axios
+      .post(`/api/v1/themes/search`, data)
+      .then((res) => setThemeCardsList(res.data.content));
+  }, [selectedWhom, selectedWhat, selectedWhere, submitKeyword]);
+
   return (
     <>
       <div className="padding-container">
-        <div className=" mb-[30px]">
-          <SearchBar />
+        <div className="mb-[30px]">
+          <SearchBar
+            inputKeyword={inputKeyword}
+            handleInputChange={(e) => setInputKeyword(e.target.value)}
+            handleInputSubmit={() => setSubmitKeyword(inputKeyword)}
+          />
         </div>
         <div className="mb-[30px]">
           <Tag
@@ -49,23 +70,24 @@ function Search() {
         </div>
         <div className="mb-[30px]">
           <div className="mb-3 font-semibold text-sm leading-none">
-            6개의 테마를 찾았어요.
+            {themeCardsList.length === 0
+              ? "검색된 테마가 없습니다. "
+              : `${themeCardsList.length}개의 테마를 찾았어요.`}
           </div>
           <ul className="grid grid-cols-2 gap-[11px]">
-            {popularThemes.map((card) => {
-              const { id, emoji, name, count } = card;
-              const option = `${count}개의 추천 장소`;
-              return (
+            {themeCardsList.map(
+              ({ themeId, themeEmoji, themeTitle, reviewCount }) => (
                 <Card
-                  key={id}
-                  id={id}
-                  emoji={emoji}
-                  name={name}
-                  option={option}
+                  key={themeId}
+                  id={themeId}
+                  emoji={themeEmoji}
+                  name={themeTitle}
+                  option={`${reviewCount}개의 추천 장소`}
                   styles={gridThemeCard}
+                  path={`theme/${themeId}`}
                 />
-              );
-            })}
+              )
+            )}
           </ul>
         </div>
         <div>
