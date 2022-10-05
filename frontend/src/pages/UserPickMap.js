354760d32fda95bbbc21d459c0591e0198d684fe
mellomello-made <mellomello.made@gmail.com>
2022-10-05 12:02:08 +0900

    유저지도(추천)에 지도 불러오기

--- frontend/src/pages/UserPickMap.js
+++ frontend/src/pages/UserPickMap.js
@@ -1,5 +1,14 @@
+import React from "react";
+import { Map } from "react-kakao-maps-sdk";
+
 function UserPickMap() {
-  return <div>UserPickMap</div>;
+  return (
+    <Map
+      center={{ lat: 37.566769, lng: 126.978323 }}
+      style={{ width: "100%", height: "100vh" }}
+      level={9}
+    ></Map>
+  );
 }
 
 export default UserPickMap;
