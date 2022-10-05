ff795eebb9f61dda8f1bec3dea0b1696c7e6f749
Jungyun park <104332913+mellomello-made@users.noreply.github.com>
2022-10-05 15:14:51 +0900

    Feat/fe/map 0004 (#184)
    * FEAT. 테마지도 페이지 지도 불러오고 마커 생성
    
    * FIX: github 액션 수정
    
    * 테마지도 페이지에 지도 불러오기
    
    * 유저지도(추천)에 지도 불러오기
    
    * 유저지도(찜)에 지도 불러오기
    
    * 추천장소 검색 페이지에 지도 불러오기

--- frontend/src/pages/SearchStore.js
+++ frontend/src/pages/SearchStore.js
@@ -1,5 +1,14 @@
+import React from "react";
+import { Map } from "react-kakao-maps-sdk";
+
 function SearchStore() {
-  return <div>SearchStore</div>;
+  return (
+    <Map
+      center={{ lat: 37.566769, lng: 126.978323 }}
+      style={{ width: "100%", height: "100vh" }}
+      level={9}
+    ></Map>
+  );
 }
 
 export default SearchStore;
