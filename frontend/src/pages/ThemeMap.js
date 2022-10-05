093728eac8a6a5084947162a6528992d001a9498
mellomello-made <mellomello.made@gmail.com>
2022-10-05 10:55:51 +0900

    테마지도 페이지에 지도 불러오기

--- frontend/src/pages/ThemeMap.js
+++ frontend/src/pages/ThemeMap.js
@@ -1,5 +1,13 @@
-function ThemeMap() {
-  return <div>ThemeMap</div>;
-}
+import React from "react";
+import { Map } from "react-kakao-maps-sdk";
 
+function ThemeMap() {
+  return (
+    <Map
+      center={{ lat: 37.566769, lng: 126.978323 }}
+      style={{ width: "100%", height: "100vh" }}
+      level={9}
+    ></Map>
+  );
+}
 export default ThemeMap;
